62849e0efcc29b70beff363e35f7cc495b137724
oubenali mohamed <oubenali2010@hotmail.fr>
2022-01-29 12:43:47 +0100

    ajout du css pour la galerie photo

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -1,21 +1,28 @@
 function mediaFactory(data) {
-  const { title, image, likes } = data
+  const { title, image, likes } = data;
 
-  const pictureMedia = `assets/images/${image}`
+  const pictureMedia = `assets/images/${image}`;
 
   function getMedia() {
-    const divMedia = document.createElement('div')
-    const imgMedia = document.createElement('img')
-    imgMedia.classList = 'media_photographe'
-    imgMedia.setAttribute('src', pictureMedia)
-    const paraMedia = document.createElement('p')
-    paraMedia.textContent = title
-    const pLike = document.createElement('p')
-    pLike.textContent = likes
-    divMedia.appendChild(imgMedia)
-    divMedia.appendChild(paraMedia)
-    divMedia.appendChild(pLike)
-    return divMedia
+    const divMedia = document.createElement("div");
+    const imgMedia = document.createElement("img");
+    imgMedia.classList = "media_photographe";
+    imgMedia.setAttribute("src", pictureMedia);
+    const linkMedia = document.createElement("a");
+    linkMedia.setAttribute("href");
+    linkMedia.appendChild(imgMedia);
+    const paraMedia = document.createElement("p");
+    paraMedia.textContent = title;
+    const pLike = document.createElement("p");
+    pLike.classList = "like";
+    pLike.textContent = likes;
+    const heart = document.createElement("i");
+    heart.classList = "fas fa-heart";
+    pLike.appendChild(heart);
+    divMedia.appendChild(linkMedia);
+    divMedia.appendChild(paraMedia);
+    divMedia.appendChild(pLike);
+    return divMedia;
   }
-  return { getMedia }
+  return { getMedia };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,41 +1,42 @@
 //Mettre le code JavaScript lié à la page photographer.html
-let urlPhotographer = window.location.search
-let searchParams = new URLSearchParams(urlPhotographer)
-let id = searchParams.get('id')
-let url_Photographer = 'data/photographers.json'
+let urlPhotographer = window.location.search;
+let searchParams = new URLSearchParams(urlPhotographer);
+let id = searchParams.get("id");
+let url_Photographer = "data/photographers.json";
 
 fetch(url_Photographer)
   .then((response) => {
-    return response.json()
+    return response.json();
   })
   .then((data) => {
-    console.log(data.photographers)
+    console.log(data.photographers);
     for (i = 0; i < data.photographers.length; i++) {
       if (id == data.photographers[i].id) {
-        const photographer = data.photographers[i]
-        const profilHeader = document.getElementById('profil')
-        console.log(photographer)
-        const imgProfil = document.createElement('div')
-        imgProfil.innerHTML = `<img class = "imgProfil" src = "assets/photographers/${photographer.portrait}">`
-        const profil = document.createElement('div')
-        profil.id = 'nom_profil'
+        const photographer = data.photographers[i];
+        const profilHeader = document.getElementById("profil");
+        console.log(photographer);
+        const imgProfil = document.createElement("div");
+        imgProfil.innerHTML = `<img class = "imgProfil" src = "assets/photographers/${photographer.portrait}">`;
+        const profil = document.createElement("div");
+        profil.id = "nom_profil";
         profil.innerHTML = `
         <h1>${photographer.name}</h1> </br> 
         <h2>${photographer.city} + ${photographer.country}</h2> </br>
         <p>${photographer.tagline}</p>
-       `
-        profilHeader.after(profil)
-        profilHeader.appendChild(imgProfil)
+       `;
+        profilHeader.after(profil);
+        profilHeader.appendChild(imgProfil);
       }
     }
+
     for (i = 0; i < data.media.length; i++) {
       if (id == data.media[i].photographerId) {
-        const media = data.media[i]
-        const photographeMedia = document.getElementById('photographeMedia')
-        const mediaModel = mediaFactory(media)
-        const mediaCardDOM = mediaModel.getMedia()
-        console.log(mediaCardDOM)
-        photographeMedia.appendChild(mediaCardDOM)
+        const media = data.media[i];
+        const photographeMedia = document.getElementById("photographeMedia");
+        const mediaModel = mediaFactory(media);
+        const mediaCardDOM = mediaModel.getMedia();
+        console.log(mediaCardDOM);
+        photographeMedia.appendChild(mediaCardDOM);
       }
     }
     /*  for (i = 0; i < data.media.length; i++) {
@@ -51,4 +52,4 @@ fetch(url_Photographer)
         main.appendChild(media_photographe)
       }
     } */
-  })
+  });
