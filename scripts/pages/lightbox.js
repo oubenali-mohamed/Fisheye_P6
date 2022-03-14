7bba607c751284e69f823bc1b65e89de9849f7b5
oubenali mohamed <oubenali15@gmail.com>
2022-03-14 22:56:19 +0100

    implémentation de la fonction suivant précédent de la lightbox

--- scripts/pages/lightbox.js
+++ scripts/pages/lightbox.js
@@ -5,6 +5,7 @@ window.onload = () => {
   const next = document.querySelector(".lightbox_next");
   const prev = document.querySelector(".lightbox_prev");
   const container_img = document.querySelector(".lightbox_container");
+  let index = 0;
   console.log(links);
 
   for (i = 0; i < links.length; i++) {
@@ -12,30 +13,35 @@ window.onload = () => {
     if (link.firstChild.nodeName === "IMG") {
       link.addEventListener("click", function (e) {
         e.preventDefault();
-        lightbox.style.display = "block";
-        const image = link.innerHTML;
-        container_img.innerHTML = image;
+        function imageLightbox() {
+          lightbox.style.display = "block";
+          const image = link.innerHTML;
+          container_img.innerHTML = image;
+        }
+        imageLightbox();
       });
     } else {
       const video_element = link.lastChild;
       if (video_element.nodeName === "VIDEO") {
         link.addEventListener("click", function (e) {
           e.preventDefault();
-          lightbox.style.display = "block";
-          const video = document.createElement("video");
-          const sourceVideo = document.createElement("source");
-          sourceVideo.setAttribute(
-            "src",
-            video_element.firstChild.attributes[0].nodeValue
-          );
-          sourceVideo.setAttribute("type", "video/mp4");
-          video.appendChild(sourceVideo);
-          video.play();
-          console.log(video);
-          if (container_img.firstChild != null) {
-            container_img.removeChild(container_img.firstChild);
+          function videoLightbox() {
+            lightbox.style.display = "block";
+            const video = document.createElement("video");
+            const sourceVideo = document.createElement("source");
+            sourceVideo.setAttribute(
+              "src",
+              video_element.firstChild.attributes[0].nodeValue
+            );
+            sourceVideo.setAttribute("type", "video/mp4");
+            video.appendChild(sourceVideo);
+            video.play();
+            if (container_img.firstChild != null) {
+              container_img.removeChild(container_img.firstChild);
+            }
+            container_img.appendChild(video);
           }
-          container_img.appendChild(video);
+          videoLightbox();
         });
       }
     }
@@ -43,10 +49,21 @@ window.onload = () => {
 
   next.addEventListener("click", function (e) {
     e.preventDefault();
+    if (index >= links.length - 1) index = -1;
+    index--;
+    const link = links[i];
+    const image = link.innerHTML;
+    container_img.innerHTML = image;
   });
 
   prev.addEventListener("click", function (e) {
     e.preventDefault();
+    if ((index <= 0, (index = links.length), i--)) {
+      const link = links[i];
+      lightbox.style.display = "block";
+      const image = link.innerHTML;
+      container_img.innerHTML = image;
+    }
   });
   close.addEventListener("click", function (e) {
     e.preventDefault();
--- scripts/pages/likes.js
+++ scripts/pages/likes.js
@@ -1,11 +1,21 @@
-window.onload = () => {
+/* window.onload = () => {
   const heart_like = document.getElementsByClassName("fas fa-heart");
   let number_like = document.getElementsByClassName("total_like");
   console.log(number_like);
   console.log(heart_like);
   console.log(heart_like.length);
 
+  function getNumberLike() {
+    for (i = 0; i < number_like.length; i++) {
+      let like_Of_Media = number_like[i].firstChild.nodeValue;
+      console.log(like_Of_Media);
+    }
+  }
+
   for (i = 0; i < heart_like.length; i++) {
-    heart_like[i].addEventListener("click", function (e) {});
+    heart_like[i].addEventListener("click", function (e) {
+      getNumberLike();
+    });
   }
 };
+ */
