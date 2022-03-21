bba5441fad3618248a2806be7fbb395037df1a9e
oubenali mohamed <oubenali15@gmail.com>
2022-03-21 10:26:45 +0100

    implementation du like

--- scripts/pages/lightbox.js
+++ scripts/pages/lightbox.js
@@ -5,65 +5,63 @@ window.onload = () => {
   const next = document.querySelector(".lightbox_next");
   const prev = document.querySelector(".lightbox_prev");
   const container_img = document.querySelector(".lightbox_container");
-  let index = 0;
   console.log(links);
 
-  for (i = 0; i < links.length; i++) {
+  function setImage() {
     const link = links[i];
     if (link.firstChild.nodeName === "IMG") {
       link.addEventListener("click", function (e) {
         e.preventDefault();
-        function imageLightbox() {
-          lightbox.style.display = "block";
-          const image = link.innerHTML;
-          container_img.innerHTML = image;
-        }
-        imageLightbox();
+        //console.log(i);
+        lightbox.style.display = "block";
+        const image = link.innerHTML;
+        container_img.innerHTML = image;
       });
     } else {
       const video_element = link.lastChild;
       if (video_element.nodeName === "VIDEO") {
         link.addEventListener("click", function (e) {
           e.preventDefault();
-          function videoLightbox() {
-            lightbox.style.display = "block";
-            const video = document.createElement("video");
-            const sourceVideo = document.createElement("source");
-            sourceVideo.setAttribute(
-              "src",
-              video_element.firstChild.attributes[0].nodeValue
-            );
-            sourceVideo.setAttribute("type", "video/mp4");
-            video.appendChild(sourceVideo);
-            video.play();
-            if (container_img.firstChild != null) {
-              container_img.removeChild(container_img.firstChild);
-            }
-            container_img.appendChild(video);
+          lightbox.style.display = "block";
+          const video = document.createElement("video");
+          const sourceVideo = document.createElement("source");
+          sourceVideo.setAttribute(
+            "src",
+            video_element.firstChild.attributes[0].nodeValue
+          );
+          sourceVideo.setAttribute("type", "video/mp4");
+          video.appendChild(sourceVideo);
+          video.play();
+          if (container_img.firstChild != null) {
+            container_img.removeChild(container_img.firstChild);
           }
-          videoLightbox();
+          container_img.appendChild(video);
         });
       }
     }
   }
-
+  for (i = 0; i < links.length; i++) {
+    setImage();
+  }
+  let index = 0;
   next.addEventListener("click", function (e) {
     e.preventDefault();
-    if (index >= links.length - 1) index = -1;
-    index--;
-    const link = links[i];
-    const image = link.innerHTML;
-    container_img.innerHTML = image;
+    console.log(index);
+    if (index >= links.length - 1) {
+      index = -1;
+    }
+    index++;
+    //return setImage();
   });
 
   prev.addEventListener("click", function (e) {
     e.preventDefault();
-    if ((index <= 0, (index = links.length), i--)) {
-      const link = links[i];
-      lightbox.style.display = "block";
-      const image = link.innerHTML;
-      container_img.innerHTML = image;
+    console.log(index);
+    if (index <= 0) {
+      index = links.length;
     }
+    index--;
+    //return setImage();
   });
   close.addEventListener("click", function (e) {
     e.preventDefault();
